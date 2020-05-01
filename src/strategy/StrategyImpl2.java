6a3f5e641030c41c8a3be5bf4e5bc3b3b89c3930
b9m <djopovitch@gmail.com>
2020-05-01 05:30:18 +0200

    The Strategy Design Pattern Implementation

---
+++ src/strategy/StrategyImpl2.java
@@ -0,0 +1,10 @@
+package strategy;
+
+public class StrategyImpl2 implements IStrategy{
+
+	@Override
+	public void applyStrategy() {
+		System.out.println("Etape Intermediare de l'algo avec la strategie 2");		
+	}
+
+}
---
+++ src/strategydesignpattern/App.java
@@ -0,0 +1,42 @@
+package strategydesignpattern;
+
+import java.util.Scanner;
+
+import strategy.IStrategy;
+import strategy.StrategyImpl1;
+import strategy.StrategyImpl2;
+import strategy.StrategyImpl3;
+
+public class App {
+	public static void main(String[] args) throws Exception{
+		System.out.println("---------------Version Statique-----------------");
+		Context ctx = new Context();
+		ctx.process();
+		System.out.println("--------------------------------");
+		ctx.setStrategy(new StrategyImpl1());
+		ctx.process();
+		System.out.println("--------------------------------");
+		ctx.setStrategy(new StrategyImpl2());
+		ctx.process();
+		System.out.println("--------------------------------");
+		ctx.setStrategy(new StrategyImpl3());
+		ctx.process();
+		
+		System.out.println("---------------Version Dynamique-----------------");
+
+		boolean fin = false ;
+		Scanner scanner = new Scanner(System.in);
+		while (!fin) {
+			System.out.print("Donner la strategie (Int 1,2,3) :");
+			int sl = scanner.nextInt();
+			String strategyClassName = "strategy.StrategyImpl"+sl;
+			//System.out.println(strategyClassName);
+			IStrategy strategyDynamique = (IStrategy) Class.forName(strategyClassName).newInstance();
+			ctx.setStrategy(strategyDynamique);
+			System.out.println("--------------------------------");
+			ctx.process();
+			
+		}
+	}
+
+}
